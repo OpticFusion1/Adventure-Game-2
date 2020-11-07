a36bdeb4261add1cf88e7c8f59026ec7266f4646
Sibusiso <itsbudamain.05@gmail.com>
2020-11-07 21:47:09 +0200

    Finite state machine to implement game logic

---
+++ FSM.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class FSM {
+
+	private State[] states;
+	private State currentState;
+	private int currentStateIndex;
+
+	private int[][] transitionTable;
+	private List<String> trantitionVerbs;
+
+	public FSM(State[] states, int[][] transitionTable, String[] eventVerbs) {
+
+	}
+
+
+} //class
