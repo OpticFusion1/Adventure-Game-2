bcd300bf4a42faf34cb29c94660eb41e14f11e2c
Sibusiso <itsbudamain.05@gmail.com>
2020-11-07 21:46:32 +0200

    main program to test everthing

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.* ;
+
+public class Main {
+
+	public static void main(String[] args) {
+			
+		List<String> stuff = new ArrayList<>();
+		stuff.add("Chicken");
+		// stuff.add("")
+
+		System.out.println(stuff);
+
+	}
+}
